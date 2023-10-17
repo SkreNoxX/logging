58b56a76d43fe662927147060aed87eb8a067973
SkreNoxX <116833316+SkreNoxX@users.noreply.github.com>
2023-10-17 21:39:17 +0300

    Add files via upload

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -4,6 +4,9 @@
 #include <array>
 #include <random>
 #include <cstdlib>
+#include <fstream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -20,12 +23,12 @@ public:
 	Logger();
 	Logger(Level levelError) { level = levelError; }
 	void setLevel(Level levelError) { level = levelError; }
+	Level getLevel() const { return level; }
 
-	void printEndlessLogger() {
+	void printEndlessLogger() { // доделать
 		srand((unsigned)time(0));
-		int number;
 		while (true) {
-			switch (number = rand() % (level + 1)) {
+			switch (rand() % (level + 1)) {
 				case 0:
 					cout << "TRACE\n";
 					break;
@@ -44,32 +47,119 @@ public:
 			}
 		}
 	}
-	void LOGE(string message) {
-		SYSTEMTIME time;
-		GetLocalTime(&time);
-		int number;
-		switch (number = rand() % (level + 1)) {
+	void LogeInConsole(string message) {
+		this_thread::sleep_for(chrono::milliseconds(1000));
+		SYSTEMTIME sysTime;
+		GetLocalTime(&sysTime);
+		srand((unsigned)time(0));
+		switch (rand() % (level + 1)) {
 			case 0:
-				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
-					 << "TRACE -> " << message << endl;
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay 
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "<< "TRACE -> " << message << endl;
 				break;
 			case 1:
-				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
 					<< "DEBUG -> " << message << endl;
 				break;
 			case 2:
-				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
 					<< "INFO -> " << message << endl;
 				break;
 			case 3:
-				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
 					<< "WARNING -> " << message << endl;
 				break;
 			case 4:
-				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
 					<< "ERROR -> " << message << endl;
 				break;
 		}
+	}
+	void LogInFile(string message, string pathOfFile) {
+		this_thread::sleep_for(chrono::milliseconds(1000));
+		SYSTEMTIME sysTime;
+		GetLocalTime(&sysTime);
+		srand((unsigned)time(0));
+
+		ofstream fout;
+		fout.open(pathOfFile, fstream::app);
+
+		if (fout.is_open()) {
+			switch (rand() % (level + 1)) {
+				case 0:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "TRACE -> " << message << "\n";
+					break;
+				case 1:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "DEBUG -> " << message << "\n";
+					break;
+				case 2:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "INFO -> " << message << "\n";
+					break;
+				case 3:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "WARNING -> " << message << "\n";
+					break;
+				case 4:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "ERROR -> " << message << "\n";
+					break;
+			}
+		}
+		else {
+			cout << "Ошибка с файлом лога";
+		}
+		fout.close();
+	}
+	void LogeInConsoleAndFile(string message, string pathOfFile) {
+		this_thread::sleep_for(chrono::milliseconds(1000));
+		SYSTEMTIME sysTime;
+		GetLocalTime(&sysTime);
+		srand((unsigned)time(0));
+
+		ofstream fout;
+		fout.open(pathOfFile, fstream::app);
+
+		if (fout.is_open()) {
+			switch (rand() % (level + 1)) {
+			case 0:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "TRACE -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "TRACE -> " << message << "\n";
+				break;
+			case 1:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "DEBUG -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "DEBUG -> " << message << "\n";
+				break;
+			case 2:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "INFO -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "INFO -> " << message << "\n";
+				break;
+			case 3:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "WARNING -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "WARNING -> " << message << "\n";
+				break;
+			case 4:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "ERROR -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "ERROR -> " << message << "\n";
+				break;
+			}
+		}
+		else {
+			cout << "Ошибка с файлом лога";
+		}
+		fout.close();
 	}
 private:
 	Level level;
--- Logger/main.cpp
+++ Logger/main.cpp
@@ -3,13 +3,35 @@
 #include <Windows.h>
 #include <cstdlib>
 #include "Logger.h"
+
+#include <fstream>
 using namespace std;
 
 
 int main()
 {
+    setlocale(LC_ALL, "ru");
     Logger logger(Level::ERROR_LEVEL);
-    logger.LOGE("User logged in");
-    logger.LOGE("User logged in");
-    logger.LOGE("User logged in");
+
+    int choice;
+    while (true) {
+        cout << "Выберите место вывода логов:\n(1)Консоль\n(2)Файл\n(3)Консоль и файл" << endl;
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            logger.LogeInConsole("User logged in");
+            logger.LogeInConsole("User logged in");
+            logger.LogeInConsole("User logged in");
+            break;
+        case 2:
+            logger.LogInFile("Сохранение данных в файл", "data.log");
+            logger.LogInFile("Сохранение данных в файл", "data.log");
+            logger.LogInFile("Сохранение данных в файл", "data.log");
+            break;
+        case 3:
+            logger.LogeInConsoleAndFile("Сохранение в консоль и в файл", "data.log");
+            break;
+        }
+    }
 }

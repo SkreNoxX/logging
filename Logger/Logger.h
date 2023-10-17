a1464fa7bc33d51dfe82a4ffc463a209e8954d1d
SkreNoxX <116833316+SkreNoxX@users.noreply.github.com>
2023-10-17 15:52:44 +0300

    Add files via upload

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <iostream>
+#include <Windows.h>
 #include <array>
 #include <random>
 #include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
 static enum Level {
-	TRACE,
-	DEBUG,
-	INFO,
-	WARNING,
-	ERROR
+	TRACE_LEVEL,
+	DEBUG_LEVEL,
+	INFO_LEVEL,
+	WARNING_LEVEL,
+	ERROR_LEVEL
 };
 
 class Logger {
@@ -25,26 +25,50 @@ public:
 		srand((unsigned)time(0));
 		int number;
 		while (true) {
-			number = rand() % 5;
-			if (number <= level) {
-				switch (number) {
-					case 0:
-						cout << "TRACE\n";
-						break;
-					case 1:
-						cout << "DEBUG\n";
-						break;
-					case 2:
-						cout << "INFO\n";
-						break;
-					case 3:
-						cout << "WARNING\n";
-						break;
-					case 4:
-						cout << "ERROR\n";
-						break;
-				}
+			switch (number = rand() % (level + 1)) {
+				case 0:
+					cout << "TRACE\n";
+					break;
+				case 1:
+					cout << "DEBUG\n";
+					break;
+				case 2:
+					cout << "INFO\n";
+					break;
+				case 3:
+					cout << "WARNING\n";
+					break;
+				case 4:
+					cout << "ERROR\n";
+					break;
 			}
+		}
+	}
+	void LOGE(string message) {
+		SYSTEMTIME time;
+		GetLocalTime(&time);
+		int number;
+		switch (number = rand() % (level + 1)) {
+			case 0:
+				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+					 << "TRACE -> " << message << endl;
+				break;
+			case 1:
+				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+					<< "DEBUG -> " << message << endl;
+				break;
+			case 2:
+				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+					<< "INFO -> " << message << endl;
+				break;
+			case 3:
+				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+					<< "WARNING -> " << message << endl;
+				break;
+			case 4:
+				cout << time.wYear << "-" << time.wMonth << "-" << time.wDay << " " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << " | "
+					<< "ERROR -> " << message << endl;
+				break;
 		}
 	}
 private:
--- Logger/main.cpp
+++ Logger/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
 #include <array>
+#include <Windows.h>
+#include <cstdlib>
 #include "Logger.h"
 using namespace std;
 
 
 int main()
 {
-    Logger logger(Level::ERROR);
-    logger.printEndlessLogger();
+    Logger logger(Level::ERROR_LEVEL);
+    logger.LOGE("User logged in");
+    logger.LOGE("User logged in");
+    logger.LOGE("User logged in");
 }

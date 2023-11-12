0364f7a84dfd714e78fc98be82e3519072522709
SkreNoxX <116833316+SkreNoxX@users.noreply.github.com>
2023-11-12 19:50:15 +0300

    Add files via upload

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,8 +1,124 @@
-#include <iostream>
-#include <Windows.h>
-#include <array>
-#include <random>
-#include <cstdlib>
-#include <fstream>
-#include <thread>
-#include <chrono>
+#include "Logger.h"
+
+Logger::Logger() { }
+Logger::Logger(Level levelError)
+	: level(levelError) {};
+Logger::Logger(Level levelError, Target targetError) { level = levelError; target = targetError; }
+void Logger::setLevel(Level levelError) { level = levelError; }
+Level Logger::getLevel() const { return level; }
+void Logger::setTarget(Target targetError) { target = targetError; }
+
+void Logger::changeFileLogeName(const string fileName) {
+	if (strlen(fileName.c_str()) < 5) {
+		cout << "Слишком короткое название\n";
+	}
+	else if (fileName[strlen(fileName.c_str()) - 1] != 'g' && fileName[strlen(fileName.c_str() - 2)] != 'o' && fileName[strlen(fileName.c_str() - 3)] != 'l' && fileName[strlen(fileName.c_str() - 4)] != '.') {
+		cout << "Необходимо указать имя файла c форматом (.log)\n";
+		cout << fileName[strlen(fileName.c_str()) - 1] << endl;
+		cout << fileName[strlen(fileName.c_str()) - 2] << endl;
+		cout << fileName[strlen(fileName.c_str()) - 3] << endl;
+		cout << fileName[strlen(fileName.c_str()) - 4] << endl;
+	}
+	else {
+		logeFileName = fileName;
+		cout << "Название файла изменено, новое название файла: " << logeFileName << endl;
+	}
+}
+void Logger::write(Level levelFunc, const string message) {
+	SYSTEMTIME sysTime;
+	GetLocalTime(&sysTime);
+	if (target == LOGE_CONSOLE && levelFunc <= level) {
+		switch (levelFunc) {
+			case 0:
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "TRACE -> " << message << endl;
+				break;
+			case 1:
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "DEBUG -> " << message << endl;
+				break;
+			case 2:
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "INFO -> " << message << endl;
+				break;
+			case 3:
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "WARNING -> " << message << endl;
+				break;
+			case 4:
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
+					<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "ERROR -> " << message << endl;
+				break;
+		}
+	}
+	else if (target == LOGE_FILE) {
+		ofstream fout;
+		fout.open(logeFileName, fstream::app);
+
+		if (fout.is_open() && levelFunc <= level) {
+			switch (levelFunc) {
+				case 0:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "TRACE -> " << message << "\n";
+					break;
+				case 1:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "DEBUG -> " << message << "\n";
+					break;
+				case 2:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "INFO -> " << message << "\n";
+					break;
+				case 3:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "WARNING -> " << message << "\n";
+					break;
+				case 4:
+					fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+						<< "ERROR -> " << message << "\n";
+					break;
+			}
+		}
+		fout.close();
+	}
+	else if (target == LOGE_CONSOLE_AND_FILE) {
+		ofstream fout;
+		fout.open(logeFileName, fstream::app);
+
+		if (fout.is_open() && levelFunc <= level) {
+			switch (levelFunc) {
+			case 0:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "TRACE -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "TRACE -> " << message << "\n";
+				break;
+			case 1:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "DEBUG -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "DEBUG -> " << message << "\n";
+				break;
+			case 2:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "INFO -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "INFO -> " << message << "\n";
+				break;
+			case 3:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "WARNING -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "WARNING -> " << message << "\n";
+				break;
+			case 4:
+				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "ERROR -> " << message << "\n";
+				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
+					<< "ERROR -> " << message << "\n";
+				break;
+			}
+		}
+		fout.close();
+	}
+}
--- Logger/Logger.h
+++ Logger/Logger.h
@@ -18,148 +18,24 @@ static enum Level {
 	ERROR_LEVEL
 };
 
+static enum Target : short {
+	LOGE_CONSOLE = 1,
+	LOGE_FILE = 2,
+	LOGE_CONSOLE_AND_FILE = 3
+};
+
 class Logger {
 public:
-	Logger() { logeFileName = "data.log"; };
-	Logger(Level levelError) {
-		level = levelError;
-		logeFileName = "data.log";
-	}
-	void setLevel(Level levelError) { level = levelError; }
-	Level getLevel() const { return level; }
-
-	void LogeInConsole(string message) {
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		SYSTEMTIME sysTime;
-		GetLocalTime(&sysTime);
-		srand((unsigned)time(0));
-		switch (rand() % (level + 1)) {
-		case 0:
-			cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay
-				<< " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | " << "TRACE -> " << message << endl;
-			break;
-		case 1:
-			cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-				<< "DEBUG -> " << message << endl;
-			break;
-		case 2:
-			cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-				<< "INFO -> " << message << endl;
-			break;
-		case 3:
-			cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-				<< "WARNING -> " << message << endl;
-			break;
-		case 4:
-			cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-				<< "ERROR -> " << message << endl;
-			break;
-		}
-	}
-	void LogInFile(string message) {
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		SYSTEMTIME sysTime;
-		GetLocalTime(&sysTime);
-		srand((unsigned)time(0));
-
-		ofstream fout;
-		fout.open(logeFileName, fstream::app);
-
-		if (fout.is_open()) {
-			switch (rand() % (level + 1)) {
-			case 0:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "TRACE -> " << message << "\n";
-				break;
-			case 1:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "DEBUG -> " << message << "\n";
-				break;
-			case 2:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "INFO -> " << message << "\n";
-				break;
-			case 3:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "WARNING -> " << message << "\n";
-				break;
-			case 4:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "ERROR -> " << message << "\n";
-				break;
-			}
-		}
-		else {
-			cout << "Ошибка с файлом лога";
-		}
-		fout.close();
-	}
-	void LogeInConsoleAndFile(string message) {
-		this_thread::sleep_for(chrono::milliseconds(1000));
-		SYSTEMTIME sysTime;
-		GetLocalTime(&sysTime);
-		srand((unsigned)time(0));
-
-		ofstream fout;
-		fout.open(logeFileName, fstream::app);
-
-		if (fout.is_open()) {
-			switch (rand() % (level + 1)) {
-			case 0:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "TRACE -> " << message << "\n";
-				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "TRACE -> " << message << "\n";
-				break;
-			case 1:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "DEBUG -> " << message << "\n";
-				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "DEBUG -> " << message << "\n";
-				break;
-			case 2:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "INFO -> " << message << "\n";
-				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "INFO -> " << message << "\n";
-				break;
-			case 3:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "WARNING -> " << message << "\n";
-				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "WARNING -> " << message << "\n";
-				break;
-			case 4:
-				fout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "ERROR -> " << message << "\n";
-				cout << sysTime.wYear << "-" << sysTime.wMonth << "-" << sysTime.wDay << " " << sysTime.wHour << ":" << sysTime.wMinute << ":" << sysTime.wSecond << " | "
-					<< "ERROR -> " << message << "\n";
-				break;
-			}
-		}
-		else {
-			cout << "Ошибка с файлом лога";
-		}
-		fout.close();
-	}
-
-	void changeFileLogeName(string fileName) {
-		if (strlen(fileName.c_str()) < 5) {
-			cout << "Слишком короткое название\n";
-		}
-		else if (fileName[strlen(fileName.c_str()) - 1] != 'g' && fileName[strlen(fileName.c_str() - 2)] != 'o' && fileName[strlen(fileName.c_str() - 3)] != 'l' && fileName[strlen(fileName.c_str() - 4)] != '.') {
-			cout << "Необходимо указать имя файла c форматом (.log)\n";
-			cout << fileName[strlen(fileName.c_str()) - 1] << endl;
-			cout << fileName[strlen(fileName.c_str()) - 2] << endl;
-			cout << fileName[strlen(fileName.c_str()) - 3] << endl;
-			cout << fileName[strlen(fileName.c_str()) - 4] << endl;
-		}
-		else {
-			logeFileName = fileName;
-			cout << "Название файла изменено, новое название файла: " << logeFileName << endl;
-		}
-	}
+	Logger();
+	Logger(Level);
+	Logger(Level level, Target target);
+	void setLevel(Level);
+	Level getLevel() const;
+	void setTarget(Target);
+	void write(Level level, const string message);
+	void changeFileLogeName(string);
 private:
-	Level level;
-	string logeFileName;
+	Level level = ERROR_LEVEL;
+	string logeFileName = "data.log";
+	Target target = LOGE_CONSOLE;
 };
